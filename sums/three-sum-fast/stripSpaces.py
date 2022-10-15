5f790495ed963200e3a9875439df07d3329c5473
Arthur <104280700+scarpart@users.noreply.github.com>
2022-10-15 00:03:29 -0300

    Update stripSpaces.py

--- sums/three-sum-fast/stripSpaces.py
+++ sums/three-sum-fast/stripSpaces.py
@@ -8,7 +8,7 @@ for i in ["1Kints.txt", "2Kints.txt", "4Kints.txt", "8Kints.txt",
         file.close()
 
     os.system(f"rm ~/github/ufsc/algorithms/sums/three-sum-fast/{i}")
-    os.system("touch {i}")
+    os.system(f"touch {i}")
     with open(i, "w") as to_write:
         to_write.write(text)
         to_write.close()
