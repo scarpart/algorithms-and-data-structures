0bbc88e3b3c21ce0c83204600268fa93b94f679b
scarpart <aenartzr@gmail.com>
2022-11-07 23:14:30 -0300

    added initial code for avl tree, need to finish

---
+++ trees/avl-tree/AVLTree.py
@@ -0,0 +1,95 @@
+
+
+class AVLTree():
+    
+    class Node():
+        def __init__(self, val):
+            self.right = None
+            self.left = None
+            self.val = val
+            self.parent = None
+
+    def __init__(self):
+        self.root = None
+
+    def search(self, x:int, pivot:Node) -> Node:
+        if x == pivot.val:
+            return pivot
+        else:
+            if x >= pivot.val:
+                if pivot.right is not None:
+                    return self.search(x, pivot.right)
+            else:
+                if pivot.left is not None:
+                    return self.search(x, pivot.left)
+        return pivot
+    
+    def insert(self, x:int):
+        new_node = self.Node(x)
+        
+        if self.root is None:
+            self.root = new_node
+        else:
+            pivot = self.search(x, self.root)
+            if x <= pivot.val:
+                pivot.left = new_node
+            else:
+                pivot.right = new_node
+            new_node.parent = pivot
+
+    def rebalance(self, pivot:Node) -> None:
+        while pivot is not None:
+            pass
+            # TODO: fix this
+        
+
+    def rotate(self, pivot:Node):
+        x = pivot
+        if x.parent is not None:
+            y = x.parent
+
+        if x.parent.parent is None:
+            self.root = x
+        else:
+            z = x.parent.parent
+            if y == z.left:
+                z.left = x
+            else:
+                z.right = x
+        
+        if x == y.left:
+            y.left = x.right 
+            x.right = y
+        else:
+            y.right = x.left
+            x.left = y 
+
+    def __string_inorder(self, pivot:Node):
+        if pivot is not None:
+            self.__string_inorder(pivot.left)
+            print(pivot.val, end=" ")
+            self.__string_inorder(pivot.right)
+
+    def __string_preorder(self, pivot:Node):
+        if pivot is not None:
+            print(pivot.val, end=" ")
+            self.__string_preorder(pivot.left)
+            self.__string_preorder(pivot.right)
+    
+    def __string_postorder(self, pivot:None):
+        if pivot is not None:
+            self.__string_postorder(pivot.left)
+            self.__string_postorder(pivot.right)
+            print(pivot.val, end=" ")
+
+    def inorder(self):
+        self.__string_inorder(self.root)
+        print()
+
+    def preorder(self):
+        self.__string_preorder(self.root)
+        print()
+    
+    def postorder(self):
+        self.__string_postorder(self.root)
+        print()
---
+++ trees/avl-tree/main.py
@@ -0,0 +1,13 @@
+from AVLTree import AVLTree
+
+tree = AVLTree()
+
+tree.insert(6)
+tree.insert(5)
+tree.insert(-2)
+tree.insert(7)
+tree.insert(-1)
+tree.insert(0)
+tree.insert(6)
+
+tree.postorder()
